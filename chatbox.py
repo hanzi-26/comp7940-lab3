1bef487f45b53a94c70dbdcea4f2b4def36d24c6
Barry <00~7705793+Barry26@user.noreply.gitee.com01~>
2025-02-17 18:07:00 +0800

    final version

--- chatbox.py
+++ chatbox.py
@@ -1,12 +1,35 @@
 ## this file is based on version 13.7 of python telegram chatbot
 ## and version 1.26.18 of urllib3
 ## chatbot.py
-
+import openai
+import os
 import telegram
 from telegram.ext import Updater, MessageHandler, Filters
 # The messageHandler is used for all message updatesimport configparser
 import configparser
 import logging
+import requests
+
+
+OPENAI_API_KEY = "3ce13f11-cb8f-45e3-8f86-b2f95ef508a6"
+openai.api_key = OPENAI_API_KEY
+apiKey = OPENAI_API_KEY
+basicUrl = "https://genai.hkbu.edu.hk/general/rest"
+modelName = "gpt-4-o-mini"
+apiVersion = "2024-05-01-preview"
+
+def ask_gpt4(prompt):
+    conversation = [{"role": "user", "content": prompt}]
+    url = basicUrl + "/deployments/" + modelName + "/chat/completions/?api-version=" + apiVersion
+    headers = {'Content-Type': 'application/json', 'api-key': apiKey}
+    payload = {'messages': conversation}
+    response = requests.post(url, json=payload, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        return data['choices'][0]['message']['content']
+    else:
+        return 'Error:', response
 
 def main():
     # Load your token and create an Updater for your Bot
@@ -28,7 +51,7 @@ def main():
     updater.idle()
 
 def echo(update, context):
-    reply_message =update.message.text.upper()
+    reply_message = ask_gpt4(update.message.text)
     logging.info("Update:" + str(update))
     logging.info("context:" + str(context))
     context.bot.send_message(chat_id=update.effective_chat.id, text= reply_message)
